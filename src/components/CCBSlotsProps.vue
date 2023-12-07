cd3d2945ba1794038a42a527ee8be6106f54b26c
Lon Lrn <lonlrn@Lons-MacBook-Pro.local>
2023-12-07 09:11:03 +0100

    af

---
+++ src/components/CCBSlotsProps.vue
@@ -0,0 +1,11 @@
+<script setup>
+const gedaan = "ja"
+const inderdaad = "misschien"
+const minder = "anders nooit iets"
+</script>
+<template>
+    <div>
+        <slot :autonaam="inderdaad" :ikhebnogmeer="minder"></slot>
+        <slot name="weereennaam" :zomaar="inderdaad"></slot>
+    </div>
+</template>
---
+++ src/components/DComponentAsync.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref } from 'vue'
+import { defineAsyncComponent } from 'vue'
+import CCBSlotsProps from './CCBSlotsProps.vue'
+import AAButtonCounter from './AAButtonCounter.vue';
+const AdminPage = defineAsyncComponent(() =>
+  import('./DDAAsyncDing.vue')
+)
+const AsyncComp = defineAsyncComponent({
+  // the loader function
+  loader: () => import('./DDAAsyncDing.vue'),
+
+  // A component to use while the async component is loading
+  loadingComponent: CCBSlotsProps,
+  // Delay before showing the loading component. Default: 200ms.
+  delay: 200,
+
+  // A component to use if the load fails
+  errorComponent: AAButtonCounter,
+  // The error component will be displayed if a timeout is
+  // provided and exceeded. Default: Infinity.
+  timeout: 3000
+})
+</script>
+<template>
+  a  
+  <AdminPage />
+  <AsyncComp />
+</template>
